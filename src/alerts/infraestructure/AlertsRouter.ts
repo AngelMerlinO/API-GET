c3783b514afa39feccbb7da1a04dd8b71f4aef92
UliGaMi <ulises.galvez@hotmail.com>
2023-07-10 15:29:25 -0600

    [FIX] server.ts

--- src/alerts/infraestructure/AlertsRouter.ts
+++ src/alerts/infraestructure/AlertsRouter.ts
@@ -5,5 +5,5 @@ import { createAlertController } from "./dependencies";
 
 export const alertsRouter = express.Router();
 
-alertsRouter.update("/alert", updateAlertController.run.bind(updateAlertController));
-alertsRouter.post("/alert", createAlertController.run.bind(createAlertController));
+alertsRouter.update("/", updateAlertController.run.bind(updateAlertController));
+alertsRouter.post("/", createAlertController.run.bind(createAlertController));
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { Signale } from "signale";
-
 import { usersRouter } from "./users/infrastructure/UsersRouter";
-
+import { alertsRouter } from "./alerts/infraestructure/AlertsRouter";
 const app = express();
 
 const signale = new Signale();
 
 app.use(express.json());
 app.use("/users", usersRouter);
+app.use("/alerts", alertsRouter)
 
 app.listen(3000, () => {
   signale.success("Server online in port 3000");
